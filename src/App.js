6cbde37218940dd0f5ea23c116755bbf1c185343
floydthormodson <floydthormodson@outlook.com>
2021-10-18 15:13:12 -0400

    Displays news articles

--- src/App.js
+++ src/App.js
@@ -3,46 +3,45 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import Home from './Components/Home';
+import Home from './Components/Home.js';
 import About from './Components/About.js';
-import ProfileList from './Components/ProfileList';
+import ProfileList from './Components/ProfileList.js';
+import Contacts from './Components/Contacts'
 
 
 function App() {
 
   const buttonStyle = { width: 150, backgroundColor: "green" };
 
-  let  profileComponents = { newsArticles:[
-        { 
-            title: "Local Billionaire Blocks Out Sun",
-            article: "Words"
-        },
-        { 
-            title: "New Food Critic on the Rise",
-            article: "Words2"
-        },
-        { 
-            title: "Rambunctious Youngster to jump Springfield Gorge tomorrow after 4pm",
-            article: "Words3"
-        }            
-    ],
-    contactProfiles: [
-        {
-            firstName: "Mr.",
-            lastName: "Snrub",
-            birthday: "09/15/1886 00:00:00",
-            profileImage: "https://static.wikia.nocookie.net/simpsons/images/5/5e/Snrub.jpg/revision/latest?cb=20140416021517"
-        },
-        {
-            firstName: "Willie",
-            lastName: "MacDougal",
-            birthday: "09/15/1886 00:00:00",
-            profileImage: "https://simpsons.fandom.com/wiki/William_MacDougal/Gallery?file=Willieismad.jpg"
-        }
-    ]};
 
- console.log(profileComponents.newsArticles)
-
+  const profileContent = { newsArticles:[
+    { 
+        title: "Local Billionaire Blocks Out Sun",
+        article: "Words"
+    },
+    { 
+        title: "New Food Critic on the Rise",
+        article: "Words2"
+    },
+    { 
+        title: "Rambunctious Youngster to jump Springfield Gorge tomorrow after 4pm",
+        article: "Words3"
+    }            
+],
+contactProfiles: [
+    {
+        firstName: "Mr.",
+        lastName: "Snrub",
+        birthday: "09/15/1886 00:00:00",
+        profileImage: "https://static.wikia.nocookie.net/simpsons/images/5/5e/Snrub.jpg/revision/latest?cb=20140416021517"
+    },
+    {
+        firstName: "Willie",
+        lastName: "MacDougal",
+        birthday: "09/15/1886 00:00:00",
+        profileImage: "https://simpsons.fandom.com/wiki/William_MacDougal/Gallery?file=Willieismad.jpg"
+    }
+]};
 
 
 
@@ -50,23 +49,19 @@ function App() {
     <div className="container">
       <div className = "nav-bar">
         <Stack spacing={0.5} direction="row" margin="auto">
-          <Link to="/"><Button variant="contained" sx={buttonStyle}>Home</Button></Link>
           <Link to="/about"><Button variant="contained" sx={buttonStyle}>About</Button></Link>
           <Link to="/profiles"><Button variant="contained" sx={buttonStyle}>Profiles</Button></Link>
+          <Link to="/"><Button variant="contained" sx={buttonStyle}>Home</Button></Link>
         </Stack>
       </div>
+      <div className = "side-bar">
+            <Route path="/" render={(props) => (<Contacts {...props} contacts={profileContent?.contactProfiles}/>)}/>
+      </div>
       <div className="main-display">
-        <Switch>
-          <Route className ="side-bar" path="/">
-            <Home contactProfiles={profileComponents.contactProfiles} articles={profileComponents.newsArticles}/>
-          </Route>
-          <Route path="/about">
-            <About/>
-          </Route>
-          <Route path="/profile">
-            <ProfileList/>
-          </Route>
-        </Switch>
+          <Route exact path="/" render={(props) => (<Home {...props} articles={profileContent?.newsArticles} />)}/>
+          <Route path="/about" render={(props) => (<About {...props}/>)}/>
+          <Route path="/profile" render={(props) => (<ProfileList {...props}/>)}/>
+          
       </div>
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,44 +1,21 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { autocompleteClasses } from '@mui/material';
 
 
-export default function Home(contactProfiles, articles){
+export default function Home(articles){
 
-
-
-    const myProfile= {
-        firstName: "Homer",
-        lastName: "Simpson",
-        birthday: "06/24/1962 00:00:00",
-        profileImage: "https://simpsons.fandom.com/wiki/Homer_Simpson/Gallery?file=180px-Donut_Head_Homer.jpg"
-
-    }
-
-
+    console.log('inside Home component:', articles)
 
     return(
-        <Box sx={{mx:autocompleteClasses, width: "100%"}}>
-            <Card className = "side-bar">
-                <CardMedia component="img" sx={{ width: "15vw", display: "inline-flex", mx: "auto" }}
-                    image={myProfile?.profileImage}
-                    alt="donut Homer"
-                />
-                <CardContent>
-                    <h5>Name: {myProfile.firstName} {myProfile.lastName}</h5>
-                    <h5>Birthday: {myProfile.birthday}</h5>
-                </CardContent>
-            </Card>
+        <Box sx={{mx:autocompleteClasses, width: "80%"}}>
             <Card className = "side-bar">
                 <CardContent>
-                    <h5><ul>{articles.map((element,index) => (<li>{element?.title}</li>))}</ul></h5>
+                    <h2>Your News</h2>
+                    <h3><ul>{articles.articles.map((element,index) => (<li key={index}>{element.title}<h5>{element.article}</h5></li>))}</ul></h3>
                 </CardContent>
             </Card>
-
         </Box>
     )
-
 }
